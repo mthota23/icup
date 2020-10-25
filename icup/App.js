40044b9b2a6d95ceb08fc16ac452cdce3b89a4ff
Sam Wang <samuel.joshua.wang@gmail.com>
2020-10-25 00:19:27 -0500

    Initial Screen Setup

--- icup/App.js
+++ icup/App.js
@@ -4,17 +4,21 @@ import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import Login from "./screens/Login";
+import Home from "./screens/Home";
 
 const Stack = createStackNavigator();
 
-export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Login" component={Login} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="Login" component={Login} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
---
+++ icup/screens/Home.js
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+import { Dimensions, Image, StyleSheet, Text, View } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+
+const { width, height } = Dimensions.get("window");
+
+class Home extends Component {
+  render() {
+    return (
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: "white",
+          justifyContent: "flex-end",
+        }}
+      >
+        <View style={{ ...StyleSheet.absoluteFill }}>
+          <Image
+            source={require("../images/sharingan.jpg")}
+            style={{ flex: 1, height: null, width: null }}
+          />
+        </View>
+        <View style={{ height: height / 3, justifyContent: "center" }}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => this.props.navigation.navigate("Login")}
+          >
+            <Text style={{ fontSize: 20, fontWeight: "bold" }}>SIGN IN</Text>
+          </TouchableOpacity>
+          <View style={{ ...styles.button, backgroundColor: "#2E71DC" }}>
+            <Text style={{ fontSize: 20, fontWeight: "bold", color: "white" }}>
+              SIGN UP
+            </Text>
+          </View>
+        </View>
+      </View>
+    );
+  }
+}
+
+export default Home;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  button: {
+    backgroundColor: "white",
+    height: 70,
+    marginHorizontal: 20,
+    borderRadius: 35,
+    alignItems: "center",
+    justifyContent: "center",
+    marginVertical: 5,
+  },
+});
