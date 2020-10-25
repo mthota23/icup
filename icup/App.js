24a030766cb8444877ccec7079ee6b6ba990d9c1
thanh tran <thanhtran@thanhs-MBP.lan>
2020-10-25 04:48:23 -0500

    Created profile page

--- icup/App.js
+++ icup/App.js
@@ -5,6 +5,8 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import Login from "./screens/Login";
 import Home from "./screens/Home";
+import Dashboard from "./screens/Dashboard";
+import Profile from "./screens/Profile";
 
 const Stack = createStackNavigator();
 
@@ -13,8 +15,9 @@ export default class App extends React.Component {
     return (
       <NavigationContainer>
         <Stack.Navigator>
-          <Stack.Screen name="Home" component={Home} />
-          <Stack.Screen name="Login" component={Login} />
+          {/* <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="Login" component={Login} /> */}
+          <Stack.Screen name="Profile" component={Profile} />
         </Stack.Navigator>
       </NavigationContainer>
     );
@@ -26,6 +29,15 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
-    justifyContent: "center",
-  },
+    justifyContent: "center"
+  }
 });
+
+{
+  /* <NavigationContainer>
+<Stack.Navigator>
+  <Stack.Screen name="Home" component={Home} />
+  <Stack.Screen name="Login" component={Login} />
+</Stack.Navigator>
+</NavigationContainer> */
+}
